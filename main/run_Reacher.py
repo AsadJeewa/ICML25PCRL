0b4c49a09a366b7e155779db0e2a007c0ea94104
AsadJeewa <asad.jeewa@gmail.com>
2026-09-02 16:44:50 +0200

    feat: reacher config, wandb

--- main/run_Reacher.py
+++ main/run_Reacher.py
@@ -1,38 +1,62 @@
 import argparse
 from utils.config import Config_reacher as Config
-from utils.train import train_reacher as train
-from utils.test import test, test4, test5, test6
-testfs = {3: test, 4: test4, 5: test5, 6: test6}
+from utils.train import train_reacher as train, compute_metrics
+from utils.test import test
 from utils.plot import plot_rewards
 from utils.env import env_agent_config_reacher as env_agent_config
 import numpy as np
 import sys
-from pygmo import hypervolume
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--seed", default=1)
     parser.add_argument("--r", default=4)
     parser.add_argument("--m", default='PreCo')
+    parser.add_argument(
+        "--use_wandb",
+        action="store_true",
+        help="Enable Weights & Biases logging"
+    )
+    parser.add_argument(
+        "--project_name",
+        default="MORL-Baselines",
+        type=str,
+        help="Weights & Biases project name"
+    )
+    parser.add_argument(
+        "--save_checkpoint",
+        action="store_true",
+        help="Save checkpoints"
+    )
+
     args = parser.parse_args()
 
     cfg = Config()
     cfg.MO_algo_name = args.m
     cfg.seed = int(args.seed)
     cfg.r_dim = int(args.r)
+    cfg.use_wandb = args.use_wandb
+    cfg.project_name = args.project_name
+    cfg.save_checkpoint = args.save_checkpoint
     env, agent = env_agent_config(cfg)
 
-  
-
     best_agent, res_dic, Hs = train(cfg, env, agent)
-    res_dic, mean_rs, refs = testfs[cfg.r_dim](cfg, env, best_agent)
+    res_dic, mean_rs, refs = test(cfg, env, best_agent)
 
-    ref_point = np.zeros(cfg.r_dim)
-    hn = mean_rs / np.linalg.norm(mean_rs, axis=1).reshape(-1, 1)
-    Hr = np.diag(np.matmul(hn, np.array(refs).T))
-    hvfast = hypervolume(-np.array(mean_rs))
-    v = hvfast.compute(ref_point)
-    print("HV:", v, "HR:", Hr.sum())
+    metrics = compute_metrics(
+        mean_rs,
+        refs,
+        cfg.ref_point
+    )
+
+    print(
+        "HV:", metrics["HV"],
+        "EUM:", metrics["EUM"],
+        "Sparsity:", metrics["Sparsity"],
+        "Cardinality:", metrics["Cardinality"],
+        "ND Ratio:", metrics["Non_Dominated_Ratio"],
+        "Alignment:", metrics["Alignment"]
+    )
     print(cfg.seed, "seed")
 
     plot_rewards(res_dic['rewards'], cfg, tag="train")
@@ -40,4 +64,4 @@ def main():
     print(repr(np.array(mean_rs)), repr(np.array(refs)))
 
 if __name__ == "__main__":
-    main() 
+    main()
--- utils/config.py
+++ utils/config.py
@@ -37,33 +37,34 @@ class Config:
 
 class Config_reacher:
     def __init__(self) -> None:
-        self.env_name = "mo-reacher-v4" 
+        self.env_name = "mo-reacher-v4"
         self.ref_point = [-50.0, -50.0, -50.0, -50.0]
         self.r_dim = 4
         self.probscale = 4
-        self.new_step_api = False 
+        self.new_step_api = False
         self.algo_name = "PPO"
         self.MO_algo_name = "PreCo"
         self.mode = "train" # train or test
-        self.seed =0 
+        self.seed = 0
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
-        self.train_eps = 40 
+        self.train_eps = 40
         # self.ref_train_eps = 800
         self.total_timesteps = 1000000
         self.test_interval = 10000
         self.test_start = 10000
         self.test_eps = 5
         self.test_res = 10
-        self.max_steps = 250 
-        self.eval_eps = 5 
-        self.eval_per_episode = 40 
+        self.num_eval_weights = 100
+        self.max_steps = 250
+        self.eval_eps = 5
+        self.eval_per_episode = 10
 
-        self.gamma = 0.99 
-        self.k_epochs = 2
-        self.actor_lr = 0.0003 
-        self.critic_lr = 0.0003 
-        self.eps_clip = 0.01 # epsilon-clip
-        self.entropy_coef = 0.001#0.001 
-        self.update_freq = 250 
-        self.actor_hidden_dim = 256 
-        self.critic_hidden_dim = 256 
+        self.gamma = 0.99
+        self.k_epochs = 3
+        self.actor_lr = 0.0003
+        self.critic_lr = 0.0003
+        self.eps_clip = 0.001
+        self.entropy_coef = 0.001
+        self.update_freq = 100
+        self.actor_hidden_dim = 256
+        self.critic_hidden_dim = 256
